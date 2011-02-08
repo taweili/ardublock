be52ba82b5f92989f4beb13c65305614c89f0841
David Li <taweili@yahoo.com>
2011-02-08 22:31:27 +0800

    fix name and dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject ardublock "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject org.ardublock/block "1.0.0-SNAPSHOT"
+  :description "A Block Programming Language for Arduino"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+				 [edu.mit/openblocks "1.0-SNAPSHOT"]])
