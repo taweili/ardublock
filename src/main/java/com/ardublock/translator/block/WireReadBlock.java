36cc80159b6607ce9b8757658df739fb4f803abe
HE Qichen <heqichen@yahoo.com.cn>
2011-08-27 01:43:47 +0800

    fix I2C bug

--- src/main/java/com/ardublock/translator/block/WireReadBlock.java
+++ src/main/java/com/ardublock/translator/block/WireReadBlock.java
@@ -25,7 +25,7 @@ public class WireReadBlock extends TranslatorBlock
 	public static void setupWireEnvironment(Translator t)
 	{
 		t.addHeaderFile("Wire.h");
-		t.addDefinitionCommand("boolean __ardublockIsI2cReadOk;\n\nvoid __ardublockI2cWriteData(int devAddr, int regAddr, int value)\n{\nWire.beginTransmission(devAddr);\nWire.send(regAddr);\nWire.send(value);\nWire.endTransmission();\n}\n\nint __ardublockI2cReadData(int devAddr, int regAddr)\n{\nint data = 0;\nWire.beginTransmission(devAddr);\nWire.send(regAddr);\nWire.endTransmission();\nWire.requestFrom(devAddr);\nif (Wire.availble() > 0)\n{\n__ardublockIsI2cReadOk = true;\ndata = Wire.receive();\n}\nelse\n{\n__ardublockIsI2cReadOk = false;\n}\nreturn data;\n}\n");
+		t.addDefinitionCommand("boolean __ardublockIsI2cReadOk;\n\nvoid __ardublockI2cWriteData(int devAddr, int regAddr, int value)\n{\nWire.beginTransmission(devAddr);\nWire.send(regAddr);\nWire.send(value);\nWire.endTransmission();\n}\n\nint __ardublockI2cReadData(int devAddr, int regAddr)\n{\nint data = 0;\nchar b;\nWire.beginTransmission(devAddr);\nWire.send(regAddr);\nWire.endTransmission();\nWire.requestFrom(devAddr, 1);\nif (Wire.available() > 0)\n{\n__ardublockIsI2cReadOk = true;\nb = Wire.receive();\ndata = b;\n}\nelse\n{\n__ardublockIsI2cReadOk = false;\n}\nreturn data;\n}\n");
 		t.addSetupCommand("Wire.begin();\n__ardublockIsI2cReadOk = false;");
 	}
 
