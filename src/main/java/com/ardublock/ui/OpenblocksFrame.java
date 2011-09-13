eaa5248ec03476159f35f16b9e21d5c22309f925
David Li <taweili@yahoo.com>
2011-09-14 07:38:22 +0800

    add save/load/generate button

--- src/main/java/com/ardublock/ui/OpenblocksFrame.java
+++ src/main/java/com/ardublock/ui/OpenblocksFrame.java
@@ -1,9 +1,14 @@
 package com.ardublock.ui;
 
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
 
 import com.ardublock.core.Context;
 
@@ -18,12 +23,12 @@ public class OpenblocksFrame extends JFrame
 
 	public OpenblocksFrame()
 	{
-		this.setTitle("Ardublock");
+		this.setTitle("ArduBlock");
 		this.setSize(new Dimension(800, 600));
+		this.setLayout(new BorderLayout());
 		//put the frame to the center of screen
 		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		// this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		initOpenBlocks();
 	}
 	
@@ -33,8 +38,22 @@ public class OpenblocksFrame extends JFrame
 		
 		WorkspaceController workspaceController = context.getWorkspaceController();
 		JComponent workspace = workspaceController.getWorkspacePanel();
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout());
+		buttons.add(new JButton("Save"));
+		buttons.add(new JButton("Load"));
+		buttons.add(new JButton("Generate"));
 		
-		this.add(workspace);
-		
+		this.add(buttons, BorderLayout.NORTH);
+		this.add(workspace, BorderLayout.CENTER);
+	}
+	
+	public void didSave() {
+	}
+	
+	public void didLoad() {
+	}
+	
+	public void didGenerate() {
 	}
 }
