c3bbca588c2538274ba2ce9cd3dacf8162eb9423
HE Qichen <heqichen@yahoo.com.cn>
2011-11-13 00:45:25 +0800

    update the version string

--- src/main/java/com/ardublock/core/Context.java
+++ src/main/java/com/ardublock/core/Context.java
@@ -18,7 +18,7 @@ public class Context
 	private Set<RenderableBlock> highlightBlockSet;
 	private Set<OpenblocksFrameListener> ofls;
 	
-	final public static String APP_NAME = "ArduBlock-20111023 ";
+	final public static String APP_NAME = "ArduBlock-20111112 ";
 	//final public static String VERSION_STRING = " ";
 	
 	public static Context getContext()
