3e6098ae31550f1c9ba4ce42f48b19eeb7545997
David Li <taweili@yahoo.com>
2012-01-29 01:07:31 +0800

    complete the properties file

--- src/main/resources/com/ardublock/block/i18n.rb
+++ src/main/resources/com/ardublock/block/i18n.rb
@@ -2,4 +2,10 @@ require 'nokogiri'
 f = File.open "ardublock_def.xml"
 doc = Nokogiri::XML(f)
 bgs = doc.css "BlockGenus"
-bgs.each {|bg| print "bg.#{bg['name']}=#{bg['initlabel']}\n" }
+bgs.each do |bg|
+  if bg['initlabel'] != "bg.#{bg['name']}"
+    # print "bg.#{bg['name']}=#{bg['initlabel']}\n"
+    bg['initlabel'] = "bg.#{bg['name']}"
+  end
+end
+puts doc.to_xml(:indent => 2)
