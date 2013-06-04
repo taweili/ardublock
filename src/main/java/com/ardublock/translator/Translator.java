755476bb6022af85b2c427f67c10b6ada85a1f5b
qichen.he <qichen.he@dianping.com>
2013-06-04 15:01:42 +0800

    bugfix, definition code generate

--- src/main/java/com/ardublock/translator/Translator.java
+++ src/main/java/com/ardublock/translator/Translator.java
@@ -58,7 +58,7 @@ public class Translator
 		{
 			for (String command:definitionSet)
 			{
-				headerCommand.append("\n");
+				headerCommand.append(command + "\n");
 			}
 			headerCommand.append("\n");
 		}
