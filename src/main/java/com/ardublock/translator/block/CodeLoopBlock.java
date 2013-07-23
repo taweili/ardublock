fa1e1cc66da8ace010f0c318ed009ce7f6aa4260
daniel <daniel@nirvana.(none)>
2013-07-22 23:49:23 -0300

    New Blocks

---
+++ src/main/java/com/ardublock/translator/block/CodeLoopBlock.java
@@ -0,0 +1,24 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class CodeLoopBlock extends TranslatorBlock
+{
+	public CodeLoopBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		String ret = translatorBlock.toCode();
+		ret=ret.substring(1);
+		ret=ret.replace(ret.substring(ret.length()-1),"");
+		ret=ret+"\n";
+		return ret;
+	}
+}
