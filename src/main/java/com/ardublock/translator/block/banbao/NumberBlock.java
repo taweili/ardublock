ee9f7221044e2f020d40026a03914577360dbf4f
heqichen <heqichen@gmail.com>
2013-08-03 23:11:47 +0800

    add BanbaoIONumberBlock

---
+++ src/main/java/com/ardublock/translator/block/banbao/NumberBlock.java
@@ -0,0 +1,22 @@
+package com.ardublock.translator.block.banbao;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class NumberBlock extends TranslatorBlock
+{
+
+	public NumberBlock(Long blockId, Translator translator, String codePrefix,
+			String codeSuffix, String label) {
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		return "banbao number";
+	}
+
+}
