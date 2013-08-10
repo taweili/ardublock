8d875f420e356fd20c52c649b4d7bc990967fb41
joselorente <joselorente@hotmail.com>
2013-08-09 21:21:00 -0700

    Add serial read block

---
+++ src/main/java/com/ardublock/translator/block/SerialReadBlock.java
@@ -0,0 +1,22 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class SerialReadBlock extends TranslatorBlock
+{
+	public SerialReadBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		translator.addSetupCommand("Serial.begin(9600);");
+		
+		String ret = "Serial.read()";
+		
+		return ret;
+	}
+}
