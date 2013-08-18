a1695f00466d98443ceb6b1692e067f18f0f53ae
heqichen <heqichen@gmail.com>
2013-08-18 17:46:21 +0800

    refactor DigitalInputBlock

--- src/main/java/com/ardublock/translator/block/DigitalInputBlock.java
+++ src/main/java/com/ardublock/translator/block/DigitalInputBlock.java
@@ -18,25 +18,35 @@ public class DigitalInputBlock extends TranslatorBlock
 		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
 		if (translatorBlock instanceof NumberBlock)
 		{
-			String number;
-			number = translatorBlock.toCode();
-			translator.addInputPin(Long.parseLong(number));
-			
-			String ret = "\tdigitalRead( ";
-			ret = ret + number;
-			ret = ret + ")";
-			return codePrefix + ret + codeSuffix;
+			return generateCodeUsingNumberBlock(translatorBlock);
 		}
 		else
 		{
-			translator.addDefinitionCommand(ARDUBLOCK_DIGITAL_READ_DEFINE);
-			String ret = "__ardublockDigitalRead(";
-			
-			ret = ret + translatorBlock.toCode();
-			ret = ret + ")";
-			return codePrefix + ret + codeSuffix;
+			return generateCodeUsingNonNumberBlock(translatorBlock);
 		}
 		
+	}
+	
+	protected String generateCodeUsingNumberBlock(TranslatorBlock translatorBlock) throws SocketNullException, SubroutineNotDeclaredException
+	{
+		String number;
+		number = translatorBlock.toCode();
+		translator.addInputPin(Long.parseLong(number));
+		
+		String ret = "\tdigitalRead( ";
+		ret = ret + number;
+		ret = ret + ")";
+		return codePrefix + ret + codeSuffix;
+	}
+	
+	protected String generateCodeUsingNonNumberBlock(TranslatorBlock translatorBlock) throws SocketNullException, SubroutineNotDeclaredException
+	{
+		translator.addDefinitionCommand(ARDUBLOCK_DIGITAL_READ_DEFINE);
+		String ret = "__ardublockDigitalRead(";
+		
+		ret = ret + translatorBlock.toCode();
+		ret = ret + ")";
+		return codePrefix + ret + codeSuffix;
 	}
 
 }
