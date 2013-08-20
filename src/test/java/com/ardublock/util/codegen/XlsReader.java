bd12cbc5bcf98ea8bcb667faa0e7ac4ece91ebe9
heqichen <heqichen@gmail.com>
2013-08-21 01:02:34 +0800

    reorganize test src folder

---
+++ src/test/java/com/ardublock/util/codegen/XlsReader.java
@@ -0,0 +1,15 @@
+package com.ardublock.util.codegen;
+
+public class XlsReader
+{
+	public static void main(String args[])
+	{
+		XlsReader me = new XlsReader();
+		me.work();
+	}
+	
+	public void work()
+	{
+		this.getClass().getClassLoader().getResourceAsStream("")
+	}
+}
