5c4d20870f0fa6373d19b50afc38d7616bbea6c5
heqichen <heqichen@gmail.com>
2013-08-21 01:47:50 +0800

    now can read one cell from xls

--- src/test/java/com/ardublock/util/codegen/XlsReader.java
+++ src/test/java/com/ardublock/util/codegen/XlsReader.java
@@ -1,15 +1,32 @@
 package com.ardublock.util.codegen;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class XlsReader
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws InvalidFormatException, IOException
 	{
 		XlsReader me = new XlsReader();
 		me.work();
 	}
 	
-	public void work()
+	public void work() throws InvalidFormatException, IOException
 	{
-		this.getClass().getClassLoader().getResourceAsStream("");
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("seeedstudio/test.xls");
+		Workbook wb = WorkbookFactory.create(is);
+		Sheet sheet = wb.getSheetAt(0);
+		System.out.println(sheet.getSheetName());
+		Row row = sheet.getRow(0);
+		System.out.println(row.getFirstCellNum());
+		Cell cell = row.getCell(0);
+		System.out.println(cell.getStringCellValue());
 	}
 }
