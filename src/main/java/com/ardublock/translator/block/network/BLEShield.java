4be60bce1d2aa6afe16c67b81f631a2aca520171
David Li <taweili@yahoo.com>
2013-08-24 05:08:22 +0800

    skeleton for BLE support

---
+++ src/main/java/com/ardublock/translator/block/network/BLEShield.java
@@ -0,0 +1,20 @@
+package com.ardublock.translator.block.network;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class BLEShield extends TranslatorBlock {
+
+	public BLEShield(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException {
+		translator.addHeaderFile("SoftwareSerial.h");
+		return null;
+	}
+
+}
