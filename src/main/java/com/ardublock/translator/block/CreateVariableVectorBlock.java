184a83d1e55298071f4a6d6d5a3746ff48e41d53
David Li <taweili@yahoo.com>
2013-10-06 12:23:35 +0800

    better error message for variables

--- src/main/java/com/ardublock/translator/block/CreateVariableVectorBlock.java
+++ src/main/java/com/ardublock/translator/block/CreateVariableVectorBlock.java
@@ -1,21 +1,30 @@
 package com.ardublock.translator.block;
 
+import java.util.ResourceBundle;
+
 import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.BlockException;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class CreateVariableVectorBlock extends TranslatorBlock
 {
+	private static ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
+	
 	public CreateVariableVectorBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
 	{
 		super(blockId, translator, codePrefix, codeSuffix, label);
 	}
+	
 	@Override
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
 		String varName="";
 		TranslatorBlock name = this.getRequiredTranslatorBlockAtSocket(0);
 		TranslatorBlock size = this.getRequiredTranslatorBlockAtSocket(1);
+		if (!(name instanceof VariableFakeBlock)) {
+			throw new BlockException(blockId, uiMessageBundle.getString("ardublock.error_msg.array_var_slot"));
+		}
 		varName+=name.toCode();
 		int foo = Integer.parseInt(size.toCode());
 		varName+="[";
