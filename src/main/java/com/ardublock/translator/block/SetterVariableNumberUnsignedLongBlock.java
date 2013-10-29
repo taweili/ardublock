5eb5d873e220b3c3f7384227a32d590603e4d6ec
TonyCallear <spamz@callear.net>
2013-10-29 00:37:31 +0000

    Allow create 'unsigned long' variables
    Supports 'millis' and 'micros' working correctly

---
+++ src/main/java/com/ardublock/translator/block/SetterVariableNumberUnsignedLongBlock.java
@@ -0,0 +1,33 @@
+package com.ardublock.translator.block;
+
+import java.util.ResourceBundle;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class SetterVariableNumberUnsignedLongBlock extends TranslatorBlock
+{
+  private static ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
+  
+  public SetterVariableNumberUnsignedLongBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+  {
+    super(blockId, translator, codePrefix, codeSuffix, label);
+  }
+
+  @Override
+  public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+  {
+    TranslatorBlock tb = this.getRequiredTranslatorBlockAtSocket(0);
+    if (!(tb instanceof VariableNumberUnsignedLongBlock)) {
+      throw new BlockException(blockId, uiMessageBundle.getString("ardublock.error_msg.number_var_slot"));
+    }
+    
+    String ret = tb.toCode();
+    tb = this.getRequiredTranslatorBlockAtSocket(1);
+    ret = "\t"+ret + " = " + tb.toCode() + " ;\n";
+    return ret;
+  }
+
+}
