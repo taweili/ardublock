6ca3890f20547b7f08346158fe99f8f6daa0ad27
TonyCallear <github@callear.net>
2013-11-16 00:22:45 +0000

    LCD Display Blocks
    Add Blocks to create and control generic LCD displays. I2C and parallel interfaces.
    Fix. Header lines generated out of order (needed for above to work)
    Move 'tone' blocks to 'Pins' draw because they set a pin's state.
    Change 'Utilities' draw to 'Generic Hardware' and improve color.
    MessageBlock: add <&space> command to allow space at start or end of messages
    MessageBlock: add <&nothing> command to allow apply settings without 'printing'
    
    Correct some of my previous daft coding in old 'utilities' draw.

---
+++ src/main/java/com/ardublock/translator/block/LCD_command_lightOff_Block.java
@@ -0,0 +1,25 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class LCD_command_lightOff_Block extends TranslatorBlock {
+	
+	public LCD_command_lightOff_Block(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	//@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		
+		TranslatorBlock tb = this.getRequiredTranslatorBlockAtSocket(0);
+		String I2C_addr = tb.toCode();
+		String ret = "lcd_I2C_" + I2C_addr + ".setBacklight(LOW);\n";
+		return ret;
+	}
+	
+}
