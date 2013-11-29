f1cca7de3b2083356ce6b97e7d6c6b43cb86823b
TonyCallear <github@callear.net>
2013-11-29 19:11:30 +0000

    LCD Display bugfix
    Screen positioning error

--- src/main/java/com/ardublock/translator/block/LCD_I2C_Sainsmart_16by2_Block.java
+++ src/main/java/com/ardublock/translator/block/LCD_I2C_Sainsmart_16by2_Block.java
@@ -15,6 +15,7 @@ public class LCD_I2C_Sainsmart_16by2_Block extends TranslatorBlock {
 	//@Override
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
+		
 		TranslatorBlock tb = this.getRequiredTranslatorBlockAtSocket(1);
 		String lineNo = tb.toCode();
 		tb = this.getRequiredTranslatorBlockAtSocket(2);
@@ -22,7 +23,10 @@ public class LCD_I2C_Sainsmart_16by2_Block extends TranslatorBlock {
 		tb = this.getRequiredTranslatorBlockAtSocket(3);
 		String I2C_addr = tb.toCode();
 		
-		String ret = "lcd_I2C_" + I2C_addr + ".setCursor( " + charNo + ", " + lineNo + " );";
+		String ret = "";
+		if ( !(charNo.equals("0") && lineNo.equals("0")) ){
+			ret = "lcd_I2C_" + I2C_addr + ".setCursor( (" + charNo + ") - 1, (" + lineNo + ") - 1 );";
+		}
 		
 		tb = this.getRequiredTranslatorBlockAtSocket(0, "lcd_I2C_" + I2C_addr + ".print( ", " );\n");
 		ret += tb.toCode();
@@ -32,7 +36,6 @@ public class LCD_I2C_Sainsmart_16by2_Block extends TranslatorBlock {
 		translator.addHeaderFile("LiquidCrystal_I2C.h");
 		//											These are *NOT* Arduino pins. They indicate how the I2C interface is connected to the LCD display
 		//																									v  v  v  v  v  v  v  v									
-
 		translator.addDefinitionCommand("LiquidCrystal_I2C lcd_I2C_" + I2C_addr + "(0x" + I2C_addr + ", 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);");
 		translator.addSetupCommand("lcd_I2C_" + I2C_addr + ".begin (16, 2);");
 		translator.addSetupCommand("lcd_I2C_" + I2C_addr + ".setBacklight(HIGH);");
