99ac84ffebe16dda4b0cb9e4beb662624ec5e0cd
heqichen <heqichen@gmail.com>
2013-12-01 22:44:56 +0800

    modify analogRead to support 88bb

--- src/main/java/com/ardublock/translator/block/AnalogInputBlock.java
+++ src/main/java/com/ardublock/translator/block/AnalogInputBlock.java
@@ -1,6 +1,7 @@
 package com.ardublock.translator.block;
 
 import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.bbbb.BbbbBlock;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
@@ -14,10 +15,15 @@ public class AnalogInputBlock extends TranslatorBlock
 	@Override
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
-		String ret = "analogRead(";
+		
+		
 		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
-		ret = ret + translatorBlock.toCode();
-		ret = ret + ")";
+		String number = translatorBlock.toCode().trim();
+		String ret = String.format("bbAnalogRead(%s)", number);
+		
+		translator.addDefinitionCommand(BbbbBlock.BBBB_DEF);
+		translator.addSetupCommand("lightOnIndicator(" + number + ");");
+		
 		return codePrefix + ret + codeSuffix;
 	}
 
---
+++ src/main/java/com/ardublock/translator/block/bbbb/BbbbBlock.java
@@ -0,0 +1,66 @@
+package com.ardublock.translator.block.bbbb;
+
+public class BbbbBlock
+{
+	public static String BBBB_DEF = 
+			"int PP_PORT_LED[10] = {0, 3, 4, 5, 6, 7, 8, 9, A3, A4};\n" + 
+			"int PP_PORT_IO[10] = {1, 2, 10, 11, 12, 13, A0, A1, A2, A5};\n\n" +
+			"void lightOnIndicator(int i)\n" + 
+			"{\n" + 
+			"	if (i<1 || i>10)\n" + 
+			"	{\n" + 
+			"		return ;\n" + 
+			"	}\n" + 
+			"	--i;\n" + 
+			"	pinMode(PP_PORT_LED[i], OUTPUT);\n" + 
+			"	digitalWrite(PP_PORT_LED[i], HIGH);\n" + 
+			"}\n\n" +
+			"\n" + 
+			"void bbDigitalWrite(int i, int status)\n" + 
+			"{\n" + 
+			"	if (i<1 || i>10)\n" + 
+			"	{\n" + 
+			"		return ;\n" + 
+			"	}\n" + 
+			"	--i;\n" + 
+			"	pinMode(PP_PORT_IO[i], OUTPUT);\n" + 
+			"	digitalWrite(PP_PORT_IO[i], status);\n" + 
+			"}\n" + 
+			"\n" + 
+			"int bbAnalogRead(int i)\n" + 
+			"{\n" + 
+			"	if (i<7 || i>10)\n" + 
+			"	{\n" + 
+			"		return 0;\n" + 
+			"	}\n" + 
+			"	--i;\n" + 
+			"	return analogRead(PP_PORT_IO[i]);\n" + 
+			"}\n" + 
+			"\n" + 
+			"int bbDigitalRead(int i, int status)\n" + 
+			"{\n" + 
+			"	if (i<1 || i>6)\n" + 
+			"	{\n" + 
+			"		return 0;\n" + 
+			"	}\n" + 
+			"	--i;\n" + 
+			"	pinMode(PP_PORT_IO[i], OUTPUT);\n" + 
+			"	return digitalRead(PP_PORT_IO[i]);\n" + 
+			"}\n" + 
+			"\n" + 
+			"void bbAnalogWrite(int i, int status)\n" + 
+			"{\n" + 
+			"	if (i<3 || i>4)\n" + 
+			"	{\n" + 
+			"		return ;\n" + 
+			"	}\n" + 
+			"	--i;\n" + 
+			"	pinMode(PP_PORT_IO[i], OUTPUT);\n" + 
+			"	analogWrite(PP_PORT_IO[i], status);\n" + 
+			"}\n" + 
+			""
+			;
+	
+	
+	
+}
