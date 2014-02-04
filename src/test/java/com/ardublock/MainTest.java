bb6958d0e37dbaa27276c4547db93e718e9a4254
HE Qichen <heqichen@hac10ermba.local>
2014-02-05 00:57:54 +0800

    now test can programmatically close ArduBlock

--- src/test/java/com/ardublock/MainTest.java
+++ src/test/java/com/ardublock/MainTest.java
@@ -6,23 +6,41 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
+import com.ardublock.core.Context;
+
 public class MainTest
 {
+	private Main main;
+
 	public static void main(String args[]) throws SAXException, IOException, ParserConfigurationException
 	{
 		MainTest test = new MainTest();
 		test.setupTest();
 		test.testFiles();
+		test.teardown();
 	}
 	
+	private void teardown() {
+		main.shutdown();
+	}
+
 	public void setupTest() throws SAXException, IOException, ParserConfigurationException
 	{
-		Main me = new Main();
-		me.startArdublock();
+		main = new Main();
+		main.startArdublock();
+		
 	}
 	
 	public void testFiles() throws SAXException, IOException, ParserConfigurationException
 	{
+		String savedFiles[] = {"factorial.abp"};
 		
+		for (String savedFile : savedFiles)
+		{
+			System.out.println(savedFile);
+		}
+		
+		Context context = Context.getContext();
+		//context.loadArduBlockFile();
 	}
 }
