9c0909d26e3ae3d865d6c09a353ba3ecd1558e22
TonyCallear <github@callear.net>
2014-02-08 01:45:21 +0000

    ImproveDoWhile
    Remove not needed \t and spaces
    Say if 'test' socket empty

--- src/main/java/com/ardublock/translator/block/DoWhileBlock.java
+++ src/main/java/com/ardublock/translator/block/DoWhileBlock.java
@@ -1,6 +1,7 @@
 package com.ardublock.translator.block;
 
 import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.BlockException;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
@@ -15,20 +16,21 @@ public class DoWhileBlock extends TranslatorBlock
   @Override
   public String toCode() throws SocketNullException, SubroutineNotDeclaredException
   {
-    String ret = "\tdo \n\t{\n ";
+    String ret = "do\n{\n ";
     TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
     translatorBlock = getTranslatorBlockAtSocket(0);
     while (translatorBlock != null)
     {
-      ret = ret + "\t"+translatorBlock.toCode();
+      ret = ret + translatorBlock.toCode();
       translatorBlock = translatorBlock.nextTranslatorBlock();
     }
 
-    ret = ret + "\t}\n\twhile(\t";
+    ret = ret + "}\nwhile(";
     translatorBlock = getTranslatorBlockAtSocket(1);
+    if (translatorBlock == null) throw new SocketNullException(blockId);
     ret = ret + translatorBlock.toCode();
 
-    ret = ret + " );\n\n";
+    ret = ret + ");\n";
     return ret;
   }
 }
