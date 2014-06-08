f7999075ee4eafb5ed7d3a3c26ec82727179d7dc
David Li <taweili@yahoo.com>
2014-06-08 21:11:57 +0800

    add littlebits images

---
+++ src/main/resources/com/ardublock/block/littlebits/prep_image.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+echo "Cropping images"
+ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -crop 560x500+260+160 \"%s_1LR.jpg\" \"%s_crop.jpg\"\n", $1, $1}' | sh
+
+echo "Resize images"
+ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -resize 100x100 \"%s_crop.jpg\" \"%s.jpg\"\n", $1, $1}' | sh
+
+echo "Remove cropped images"
+rm -rf */*_crop.jpg
+
+# rm -f `ls */*1LR* | sed -e 's/_1LR.jpg$//'`
