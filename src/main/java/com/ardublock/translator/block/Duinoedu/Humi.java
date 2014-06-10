6b5571c8a71c465a2dd205feedac776ea45c5ab6
karlTH <karl_trimix@hotmail.com>
2014-06-10 23:39:41 +0200

    add keyboard, duinoedu
    add keyboard in utilities
    add duinoedu tab and 3 block in duinoedu

---
+++ src/main/java/com/ardublock/translator/block/Duinoedu/Humi.java
@@ -0,0 +1,39 @@
+package com.ardublock.translator.block.DuinoEDU;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Humi extends TranslatorBlock {
+	public Humi(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		String Pin;
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		Pin = translatorBlock.toCode();
+
+		
+		translator.addHeaderFile("DHT.h");
+		translator.addSetupCommand("monDHTPRO.begin();");
+		translator.addDefinitionCommand("DHT monDHT("+Pin +");"	);
+		
+		String ret = "monDHT.lireHumidite(); ";
+		
+
+		return codePrefix + ret + codeSuffix;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/ardublock/translator/block/Duinoedu/Infrared.java
@@ -0,0 +1,41 @@
+package com.ardublock.translator.block.DuinoEDU;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Infrared extends TranslatorBlock {
+	public Infrared(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		String Pin;
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		Pin = translatorBlock.toCode();
+
+		
+		translator.addHeaderFile("IRremote.h");
+		translator.addSetupCommand("ARD monRecepteur.enableIRIn(); ");
+		
+		translator.addDefinitionCommand("IRrecv monRecepteur("+Pin +");"	);
+		
+		
+		String ret = "EDU monRecepteur.lireCodeIr(); ";
+		
+
+		return codePrefix + ret + codeSuffix;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
