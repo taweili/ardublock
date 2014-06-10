82ee8d01ed3562b97730bb43c8d671e0727045ca
David Li <taweili@yahoo.com>
2014-06-10 20:58:59 +0800

    update images

--- src/main/resources/com/ardublock/block/littlebits/prep_image.sh
+++ src/main/resources/com/ardublock/block/littlebits/prep_image.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-echo "Cropping images"
-ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -crop 560x500+260+160 \"%s_1LR.jpg\" \"%s_crop.jpg\"\n", $1, $1}' | sh
+# echo "Cropping images"
+# ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -crop 560x500+260+160 \"%s_1LR.jpg\" \"%s_crop.jpg\"\n", $1, $1}' | sh
 
 echo "Resize images"
-ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -resize 100x100 \"%s_crop.jpg\" \"%s.jpg\"\n", $1, $1}' | sh
+ls */*1LR* | sed -e 's/_1LR.jpg$//' | awk -F: '{printf "convert -resize 100x150 \"%s_1LR.jpg\" \"%s.jpg\"\n", $1, $1}' | sh
 
 echo "Remove cropped images"
 rm -rf */*_crop.jpg
