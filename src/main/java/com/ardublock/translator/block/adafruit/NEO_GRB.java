22ca78edc5bc763db193cb98dcd20da51dada98d
karlTH <karl_trimix@hotmail.com>
2014-08-04 11:11:09 +0200

    adafruit neopixel
    add neopixel init
    add neopixel setcolor
    add neopixel show

---
+++ src/main/java/com/ardublock/translator/block/adafruit/NEO_GRB.java
@@ -0,0 +1,19 @@
+package com.ardublock.translator.block.adafruit;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+
+public class NEO_GRB extends TranslatorBlock
+{
+
+	public NEO_GRB(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException {
+		return codePrefix + "NEO_GRB" + codeSuffix;
+	}
+
+}
