8dabcdec9ba8b892dde844b543b289da488755a8
karlTH <karl_trimix@hotmail.com>
2014-10-11 09:14:42 +0200

    add pinb pinc pind
    add this bloc

---
+++ src/main/java/com/ardublock/translator/block/PINB.java
@@ -0,0 +1,21 @@
+package com.ardublock.translator.block;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class PINB extends TranslatorBlock
+{
+	public PINB(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		
+		return codePrefix + "PINB" + codeSuffix;
+	}
+
+}
