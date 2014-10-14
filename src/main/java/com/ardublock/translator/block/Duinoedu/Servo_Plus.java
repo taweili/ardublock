beeeefc189b9f73b2e335472c30de02c45cdba79
karlTH <karl_trimix@hotmail.com>
2014-10-14 11:32:11 +0200

    add servo plus in duinoedu
    you can controle the speed of the servo
    du to the lib.

---
+++ src/main/java/com/ardublock/translator/block/Duinoedu/Servo_Plus.java
@@ -0,0 +1,36 @@
+package com.ardublock.translator.block.Duinoedu;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Servo_Plus  extends TranslatorBlock {
+
+	public Servo_Plus (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	
+	//@Override
+		public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+		{
+			String Pin;
+			String Angle;
+			String Speed;
+			TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+			Pin = translatorBlock.toCode();
+			translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+			Angle = translatorBlock.toCode();
+			translatorBlock = this.getRequiredTranslatorBlockAtSocket(2);
+			Speed = translatorBlock.toCode();
+			
+			translator.addHeaderFile("Servo.h");
+			translator.addHeaderFile("ServoPlus.h");
+			translator.addDefinitionCommand("//libraries at http://www.duinoedu.com/ \n//"
+					+ "ServoPlus monServoPlus"+Pin+";");
+			translator.addSetupCommand("monServoPlus"+Pin+".brancher("+Pin+");\n");
+			
+			return codePrefix +"monServoPlus"+Pin+".ecrireAngle("+Angle+","+Speed+");"+ codeSuffix;
+		}
+}
