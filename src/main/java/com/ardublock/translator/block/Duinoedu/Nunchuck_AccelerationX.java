49a4e021b0ec49419c028e1d46fac32ed5226977
karlTH <karl_trimix@hotmail.com>
2014-10-24 11:57:27 +0200

    new nunchuck duinoedu
    add block for nunchuck

---
+++ src/main/java/com/ardublock/translator/block/Duinoedu/Nunchuck_AccelerationX.java
@@ -0,0 +1,25 @@
+package com.ardublock.translator.block.Duinoedu;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Nunchuck_AccelerationX  extends TranslatorBlock {
+
+	public Nunchuck_AccelerationX (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	
+	//@Override
+		public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+		{		
+			translator.addHeaderFile("Wire.h");
+			translator.addHeaderFile("Nunchuck.h");
+			translator.addDefinitionCommand("//libraries at http://www.duinoedu.com/");
+			translator.addSetupCommand("monNunchuck.brancher();");
+			
+			return codePrefix +"monNunchuck.lireAccelerationX()" + codeSuffix;	
+		}
+}
