caa44878b223e4b7e76922c8a06aebf9cf8e3b7c
karlTH <karl_trimix@hotmail.com>
2014-11-22 22:53:08 +0100

    guino init no need
    no need guino init, it s in translator

--- src/main/java/com/ardublock/translator/Translator.java
+++ src/main/java/com/ardublock/translator/Translator.java
@@ -132,7 +132,7 @@ public class Translator
 		
 		if (!guinoCommand.isEmpty())
 		{
-			guinoFunction.append("void GUINO_DEFINIR_INTERFACE()\n{\n");
+			guinoFunction.append("void GUINO_DEFINIR_INTERFACE()\n{\nGUINO_GERER_INTERFACE();\n");
 			for (String command:guinoCommand)
 			{
 				guinoFunction.append(command + "\n");
