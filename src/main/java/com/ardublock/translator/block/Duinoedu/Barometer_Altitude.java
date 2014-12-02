a9c8b8b4c76dbbf38437e5fb4320504df35428c3
karlTH <karl_trimix@hotmail.com>
2014-12-02 20:24:10 +0100

    link lib duinoedu
    fix a little bug note exactly the same code for
    addDefinitionCommand than the other barometer

--- src/main/java/com/ardublock/translator/block/Duinoedu/Barometer_Altitude.java
+++ src/main/java/com/ardublock/translator/block/Duinoedu/Barometer_Altitude.java
@@ -18,7 +18,7 @@ public class Barometer_Altitude  extends TranslatorBlock {
 			translator.addHeaderFile("Wire.h");
 			translator.addHeaderFile("Barometer.h");
 			translator.addDefinitionCommand("Barometer monBarometre;");
-			translator.addDefinitionCommand("//http://duinoedu.com/dl/lib/grove/EDU_BarometerSensor_Grove/");
+			translator.addDefinitionCommand("//libraries at http://duinoedu.com/dl/lib/grove/EDU_BarometerSensor_Grove/");
 			translator.addSetupCommand("monBarometre.brancher();");
 			
 			return codePrefix +"monBarometre.lireAltitude()" + codeSuffix;	
