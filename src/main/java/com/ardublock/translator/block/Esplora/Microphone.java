c30254fda9bf00a6502368a5be1b7f25e687a88c
karlTH <karl_trimix@hotmail.com>
2015-04-05 18:53:10 +0200

    add esplora microphone

---
+++ src/main/java/com/ardublock/translator/block/Esplora/Microphone.java
@@ -0,0 +1,27 @@
+package com.ardublock.translator.block.Esplora;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+
+public class Microphone extends TranslatorBlock {
+
+	public Microphone (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	//@Override
+			public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+			{
+				
+				
+				translator.addHeaderFile("Esplora.h");
+				String ret = "Esplora.readMicrophone()";
+				
+				return codePrefix + ret + codeSuffix;
+					
+			}
+	
+}
