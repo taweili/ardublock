6d745e4cf7dabf36d574e87587c2cd9fd692f7f3
karlTH <karl_trimix@hotmail.com>
2015-09-01 14:04:03 +0200

    add block to gps
    add some block for pin  connection on UNO

--- src/main/java/com/ardublock/translator/block/DuinoEDU/Uno89.java
+++ src/main/java/com/ardublock/translator/block/DuinoEDU/Uno89.java
@@ -4,16 +4,16 @@ import com.ardublock.translator.Translator;
 import com.ardublock.translator.block.TranslatorBlock;
 import com.ardublock.translator.block.exception.SocketNullException;
 
-public class Uno extends TranslatorBlock
+public class Uno89 extends TranslatorBlock
 {
 
-	public Uno(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+	public Uno89(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
 		super(blockId, translator, codePrefix, codeSuffix, label);
 	}
 
 	@Override
 	public String toCode() throws SocketNullException {
-		return codePrefix + "monGps.brancher(4,5);" + codeSuffix;
+		return codePrefix + "monGps.brancher(8,9);" + codeSuffix;
 	}
 
 }
