070ca5bab931efd1d011ac29cdbd63ce075d20a8
TonyCallear <github@callear.net>
2016-03-27 12:53:00 +0100

    Correct Servo. Description improvements

--- src/main/java/com/ardublock/translator/block/ServoSg90Block.java
+++ src/main/java/com/ardublock/translator/block/ServoSg90Block.java
@@ -1,12 +1,16 @@
 
 package com.ardublock.translator.block;
 
+import java.util.ResourceBundle;
+
 import com.ardublock.translator.Translator;
 import com.ardublock.translator.block.exception.BlockException;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class ServoSg90Block extends TranslatorBlock {
+	
+	private static ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
 
 	public ServoSg90Block(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
 	{
@@ -20,11 +24,16 @@ public class ServoSg90Block extends TranslatorBlock {
 
 		String servoSpecs = ",530,2600";
 
-		
-
 		String pinNumber = tb.toCode();
 		String servoName = "servo_pin_" + pinNumber;
 
+		//****** Bit long w but easy to see what's happening. Any other invalid pins? *********
+		if ( ! ("2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32/"
+				+ " 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53").contains(pinNumber.trim()) )
+		{
+			throw new BlockException(blockId, uiMessageBundle.getString("ardublock.error_msg.Digital_pin_slot"));
+		}
+		
 		tb = this.getRequiredTranslatorBlockAtSocket(1);
 
 		String ret = servoName + ".write( " + tb.toCode() + " );\n";
