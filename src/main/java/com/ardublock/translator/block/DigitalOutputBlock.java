706b868c5b8bb7ac5e4a5481f4383cd60f1a654c
TonyCallear <github@callear.net>
2017-04-14 14:21:57 +0100

    Pin block corrections
    Stop treating numberblocks as a special case. See issue #153
    InputPullup. Correct socket type so it can be used.
    Toggle that works with variables and maths denoting pin number.

--- src/main/java/com/ardublock/translator/block/DigitalOutputBlock.java
+++ src/main/java/com/ardublock/translator/block/DigitalOutputBlock.java
@@ -20,22 +20,22 @@ public class DigitalOutputBlock extends TranslatorBlock
 		// Should it ever not use ARDUBLOCK_DIGITAL_WRITE_DEFINE? Cannot think why.
 		// What is special about NumberBlocks?
 		// Should check for "is a simple number" not "instanceof NumberBlock" now that pinLists constants exist 
-		if (translatorBlock instanceof NumberBlock)
-		{
-			String number = translatorBlock.toCode();
-			String setupCode = "pinMode( " + number + " , OUTPUT);";
-			translator.addSetupCommand(setupCode);
-			
-			String ret = "digitalWrite( ";
-			ret = ret + number;
-			ret = ret + " , ";
-			translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
-			ret = ret + translatorBlock.toCode();
-			ret = ret + " );\n";
-			return ret;
-		}
-		else
-		{
+//		if (translatorBlock instanceof NumberBlock)
+//		{
+//			String number = translatorBlock.toCode();
+//			String setupCode = "pinMode( " + number + " , OUTPUT);";
+//			translator.addSetupCommand(setupCode);
+//			
+//			String ret = "digitalWrite( ";
+//			ret = ret + number;
+//			ret = ret + " , ";
+//			translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+//			ret = ret + translatorBlock.toCode();
+//			ret = ret + " );\n";
+//			return ret;
+//		}
+//		else
+//		{
 			translator.addDefinitionCommand(ARDUBLOCK_DIGITAL_WRITE_DEFINE);
 			String ret = "__ardublockDigitalWrite(";
 			
@@ -45,7 +45,7 @@ public class DigitalOutputBlock extends TranslatorBlock
 			ret = ret + translatorBlock.toCode();
 			ret = ret + ");\n";
 			return ret;
-		}
+//		}
 		
 	}
 
