f268f736d8fa08db0da8e5260f513e91c46a2c93
vedes <vedes2003@gmail.com>
2017-07-20 19:51:47 +0300

    It is better to make the object String thet then to apply an equals method

--- src/main/java/com/ardublock/translator/block/StringEmptyBlock.java
+++ src/main/java/com/ardublock/translator/block/StringEmptyBlock.java
@@ -15,10 +15,10 @@ public class StringEmptyBlock extends TranslatorBlock
 	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
 	{
 		StringBuilder ret = new StringBuilder();
-		ret.append("strlen(");
+		ret.append("");
 		TranslatorBlock tb = this.getRequiredTranslatorBlockAtSocket(0);
 		ret.append(tb.toCode());
-		ret.append(") == 0");
+		ret.append(".length() == 0");
 		return codePrefix + ret + codeSuffix;
 	}
 }
